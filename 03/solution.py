5a6fe527cbe754fdb1a6dee7cd263b8eb9bfaee8
Daniel Eriksson <daniel@deriksson.se>
2016-12-09 09:25:05 +0100

    Add solution for part 2 of day 3.

--- 03/solution.py
+++ 03/solution.py
@@ -5,10 +5,32 @@ def is_valid_triangle(a, b, c):
     return a + b > c and b + c > a and a + c > b
 
 
-if __name__ == '__main__':
+def part1():
     pattern = re.compile(r'\s*(\d*)\s+(\d*)\s+(\d*)')
     count = 0
     with open('input.txt') as f:
         for line in f:
             count += is_valid_triangle(*map(int, pattern.match(line).groups()))
     print(count)
+
+
+def part2():
+    pattern = re.compile(r'\s*(\d*)\s+(\d*)\s+(\d*)')
+    count = 0
+    cols = [[], [], []]
+    with open('input.txt') as f:
+        for line in f:
+            vals = map(int, pattern.match(line).groups())
+            for col, val in zip(cols, vals):
+                col.append(val)
+            if len(cols[0]) == 3:
+                count += sum(is_valid_triangle(*col) for col in cols)
+                cols = [[], [], []]
+    print(count)
+
+
+if __name__ == '__main__':
+    print('Answer part 1:')
+    part1()
+    print('Answer part 2:')
+    part2()
